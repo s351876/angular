9e3bc66939234fdd41253ad61e676dc9b3bdea4a
s351876 <darek.byq360@gmail.com>
2017-05-24 23:23:47 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   styles: [`
@@ -53,24 +55,17 @@ import { Component } from '@angular/core';
 
   selector: 'my-app',
   template: `
+  <h1>{{title}}</h1>
   <h2>My Heroes</h2>
   <ul class="heroes">
     <li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero"><span class="badge">{{hero.id}}</span> {{hero.name}}</li>
   </ul>
-  <div *ngIf="selectedHero">
-    <h2>{{selectedHero.name}} details!</h2>
-    <div><label>id: </label>{{selectedHero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-    </div>
-  </div>
+  <hero-detail [hero]="selectedHero"></hero-detail>
   `
 })
 export class AppComponent {
-  name = 'Angular';
   title = 'Tour of Heroes';
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
 
@@ -78,23 +73,4 @@ export class AppComponent {
     this.selectedHero = hero;
   }
 
-}
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
-
-
-export class Hero {
-  id: number;
-  name: string;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 import { FormsModule } from '@angular/forms'
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
-  imports:      [
+  imports: [
     BrowserModule,
     FormsModule
    ],
-  declarations: [ AppComponent ],
+   declarations: [
+     AppComponent,
+     HeroDetailComponent
+   ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
